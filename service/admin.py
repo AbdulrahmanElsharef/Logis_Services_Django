f333852e9d3d0c1fba2a41ef42d5f49cfe28676f
unknown <abdoelsharef1@gmail.com>
2023-02-23 16:17:26 +0200

    add classinline in admin

--- service/admin.py
+++ service/admin.py
@@ -3,13 +3,28 @@ from .models import *
 # Register your models here.
 
 
-admin.site.register(Service)
+class ConditionInline(admin.TabularInline):
+    model = Condition
+
+
+class OfferInline(admin.TabularInline):
+    model = Offer
+
+
+class ServiceAdmin(admin.ModelAdmin):
+    inlines = [ConditionInline]
+
+
+class PricingAdmin(admin.ModelAdmin):
+    inlines = [OfferInline]
+
+
+admin.site.register(Service, ServiceAdmin)
 admin.site.register(Condition)
 admin.site.register(LastService)
 admin.site.register(About)
 admin.site.register(OurTeam)
-admin.site.register(Pricing)
+admin.site.register(Pricing, PricingAdmin)
 admin.site.register(Offer)
 admin.site.register(Review)
 admin.site.register(FaqAsked)
-
